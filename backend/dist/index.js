5cee2ea843d333d9f71e01778b6974193f291aff
avidin91 <a.vidin.91@gmail.com>
2023-04-03 21:46:58 +0300

    test

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('Hello world ts');
+const express = require('express');
+const PORT = process.env.PORT || 9000;
+const app = express();
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello from backend'
+    });
+});
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+app.listen(PORT, () => {
+    console.log(`Server starting on port ${PORT}`);
+});
+console.log('test');
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,23 @@
 const express = require('express')
 
-const PORT = process.env.PORT || 3005
+const PORT = process.env.PORT || 9000
 
 const app = express()
 
-app.listen(PORT, () => {
-    console.log(`Server starting on port ${PORT}`)
-})
+
 
 app.get('/api', (req:any, res:any) => {
     res.json({
-        message: 'Hello from backend'
+        message: 'Hello from backend123'
     })
+})
+
+app.get('/', (req:any, res:any) => {
+    res.send('Hello world')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server starting on port ${PORT}`)
 })
 
 console.log('test')
